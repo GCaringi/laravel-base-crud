486f129e0218528f242f5ba1939d346d695ddf9b
GCaringi <giannicaringi94@gmail.com>
2022-07-14 16:24:36 +0200

    Population db

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Comic;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -11,6 +12,22 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics');
+
+        foreach($comics as $comic){
+            $tempComic = new Comic();
+
+            $tempComic->title = $comic['title'];
+            $tempComic->description = $comic['description'];
+            $tempComic->thumb = $comic['thumb'];
+            $tempComic->price = $comic['price'];
+            $tempComic->series = $comic['series'];
+            $tempComic->sale_date = $comic['sale_date'];
+            $tempComic->type = $comic['type'];
+
+            $tempComic->save();
+        }
+
+
     }
 }
